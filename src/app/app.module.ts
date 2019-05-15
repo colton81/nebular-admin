e45c6eb2ef6abf6ae212840ee6394def1190e2a1
colton81 <coltonhillebrand92@icloud.com>
2019-05-15 01:36:55 -0500

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+/* NEBULAR MODULES*/
+import { RouterModule } from '@angular/router';
+import { NbThemeModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContactComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
+    RouterModule.forRoot([]),
+    NbThemeModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
